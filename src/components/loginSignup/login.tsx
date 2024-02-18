f96bed1708a8120d91db4aef9a7b95659b87546d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-18 09:14:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/loginSignup/login.tsx
+++ src/components/loginSignup/login.tsx
@@ -1,7 +1,9 @@
 const login = () => {
   return (
-    <div className="container mx-auto px-16">
-        Login Here
+    <div className="bg-cover bg-center h-screen bg-[url(https://wallpapercave.com/wp/wMM3NrR.jpg)]">
+        <div className="container mx-auto px-16">
+            Login Here
+        </div>
     </div>
   )
 }
