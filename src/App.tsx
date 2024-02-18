6379c063e8f83a75ad8e83ae7fe9cc8dfa0efed0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-18 09:08:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/loginSignup/login";
+
 
 export default function App() {
   return (
     <BrowserRouter>
-    
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
     </BrowserRouter>
   )
 }
