006340887afdbc549919d25e043233e0d1e36d0f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-18 14:53:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/loginSignup/login.tsx
+++ src/components/loginSignup/login.tsx
@@ -4,13 +4,13 @@ const login = () => {
   return (
     <div className="bg-cover bg-center h-screen bg-[url(https://wallpapercave.com/wp/wMM3NrR.jpg)]">
         <div className="container mx-auto md:px-40 px-4 py-40">
-            <div className="rounded border border-white">
-                <div className="md:grid grid-cols-2 gap-2">
-                    <div className="backdrop-blur-xl md:py-20 md:px-20 text-white">
+            <div className="rounded md:border md:border-white">
+                <div className="md:grid grid-cols-2">
+                    <div className="backdrop-blur-xl md:py-20 md:px-20 text-white md:visible invisible">
                       <h1 className="text-3xl font-semibold">Welcome Back</h1>
                       <p className="text-xl py-2">Enterprise resource planning </p>
                     </div>
-                    <div className="py-8 md:px-16 bg-white">
+                    <div className="py-8 md:px-16 px-8 bg-white">
                       <div className="text-3xl font-semibold flex">
                         <h1 className="">Login</h1>
                         <h1 className="pt-2 pl-2"><IonIcon name="person"></IonIcon></h1>
