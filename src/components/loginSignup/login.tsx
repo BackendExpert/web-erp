60973e777b8f4e39ce86280f468bac60d9660ee8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-18 09:17:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/loginSignup/login.tsx
+++ src/components/loginSignup/login.tsx
@@ -1,8 +1,10 @@
 const login = () => {
   return (
     <div className="bg-cover bg-center h-screen bg-[url(https://wallpapercave.com/wp/wMM3NrR.jpg)]">
-        <div className="container mx-auto px-16">
-            Login Here
+        <div className="container mx-auto px-40 py-40">
+            <div className="md:px-20 md:py-20 bg-white">
+                Login Here
+            </div>
         </div>
     </div>
   )
