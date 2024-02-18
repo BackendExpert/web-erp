90e13c96b4e334457890972fa9556304556836eb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-18 14:28:22 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/loginSignup/login.tsx
+++ src/components/loginSignup/login.tsx
@@ -3,7 +3,10 @@ const login = () => {
     <div className="bg-cover bg-center h-screen bg-[url(https://wallpapercave.com/wp/wMM3NrR.jpg)]">
         <div className="container mx-auto px-40 py-40">
             <div className="md:px-20 md:py-20 bg-white rounded">
-                Login Here
+                <div className="md:grid grid-cols-2 gap-2">
+                    <div className="">Login IMG</div>
+                    <div className="">Login Form</div>
+                </div>
             </div>
         </div>
     </div>
