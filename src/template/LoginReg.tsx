efc4a09128fbb3482ab9a64b028947f563ae19d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 19:27:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/template/LoginReg.tsx
+++ src/template/LoginReg.tsx
@@ -1,4 +1,10 @@
-const loginReg = () => {
+import { ReactNode } from "react"
+
+interface Props{
+    children: ReactNode;
+}
+
+const loginReg = ({children}:Props) => {
   return (
     <div>loginReg</div>
   )
