fbc0279e02493a298e0ddd1787f2aa33645285c4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 19:36:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/template/LoginReg.tsx
+++ src/template/LoginReg.tsx
@@ -6,8 +6,10 @@ interface Props{
 
 const loginReg = ({children}:Props) => {
   return (
-    <div className="container mx-auto px-32">
-        Login
+    <div className="">
+        <div className="container mx-auto px-32">
+            Login
+        </div>
     </div>
   )
 }
