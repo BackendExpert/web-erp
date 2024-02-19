be0fa2e23199bf095825ca9e556ea9486bcb464c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 20:12:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/template/LoginReg.tsx
+++ src/template/LoginReg.tsx
@@ -6,9 +6,12 @@ interface Props{
 
 const loginReg = ({children}:Props) => {
   return (
-    <div className="bg-[(https://wallpapercave.com/wp/wMM3NrR.jpg)] w-screen h-screen bg-cover bg-center">
-        <div className="container mx-auto px-32">
+    <div className="bg-[url(https://wallpapercave.com/wp/wMM3NrR.jpg)] w-screen h-screen bg-cover bg-center">
+        <div className="container mx-auto px-32 py-40">
+            <div className="bg-white py-10">
             {children}
+            </div>
+            
         </div>
     </div>
   )
